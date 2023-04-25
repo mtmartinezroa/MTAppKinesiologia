2196456cc9513b71dca9581da6a5a90b063fe9c7
mtmartinezroa <madoka95kaname@gmail.com>
2023-04-25 07:40:24 -0400

    upload

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,19 @@ void main() {
     home: Scaffold( //Widget "Padre" que contiene a los demas
       appBar: AppBar( //Widget que contiene la barra superior de la app
         title: const Text('Menu Principal'),
-        centerTitle: true
+        centerTitle: true,
+        backgroundColor: Colors.lightBlue[400],
       ),
-      body: const Center( //Widget que contiene el cuerpo de la app
-        child: Text('Hola este es el menu principal'),
+      body: Center( //Widget que contiene el cuerpo de la app
+        child: Text(
+          'Hola este es el menu principal',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
